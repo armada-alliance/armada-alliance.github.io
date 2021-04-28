f9ccaee490ce9f3c9357e720e9cc845a6c4fbb35
ada-pi-machine <ada.pi.org@gmail.com>
2021-04-28 17:31:00 +0000

    update build

---
+++ _next/static/wj1NxX-B7xoMT5AzArgZV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ac68931164eff9ea1db9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b964352b65496cd384dd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
